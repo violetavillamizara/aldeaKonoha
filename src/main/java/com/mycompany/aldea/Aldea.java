9ad15f4c98abb4e7f7def5110e18c76a7a1210d6
user <violeta>
2024-01-29 14:00:26 -0500

    Empezando ejercicio

---
+++ src/main/java/com/mycompany/aldea/Aldea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aldea;
+
+/**
+ *
+ * @author user
+ */
+public class Aldea {
+
+    public static void main(String[] args) {
+        
+    }
+}
